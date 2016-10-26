0aa45861561e113349d24c2b89e1c66fa0aea679
JWizerd <jwodke@outlook.com>
2016-10-26 15:49:32 -0600

    add new contact text

--- CRUD_form/practice_test/read.php
+++ CRUD_form/practice_test/read.php
@@ -8,8 +8,7 @@ include('functions.php');
 <?php include('_includes/header.php'); ?>
 
 <h1>Your Contacts</h1>
-<p class="lead">view ALL your contacts here</p>
-<p><strong class="blue">OR</strong> <a class="red" href="add_number.php">Add a new contact</a></p>
+<p><a class="red" href="add_number.php">Add a new contact?</a></p>
 
 <dl><?php renderContacts(); ?></dl>
 
